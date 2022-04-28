1e6b7155650ecc6a55dcf1565bdc53a8dd0ed684
Eva Reitzel <evareitzel@gmail.com>
2022-04-27 21:29:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ function introduction() {
 
     return "Hi, my name is ${name}."
 }
-*/
 
-/*
 function logTwoValues(value1, value2) {
     console.log(`The two values are ${value1} and ${value2}.`);
   }
